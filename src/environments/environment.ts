0e58ff879f227c5bd9a23370f6aea9ebe6414c87
vickyv02 <59869011+vickyv02@users.noreply.github.com>
2026-01-07 19:54:05 +0100

    fix peer dependency conflicts in CI?

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyAyPl-BFpTI7a27OpHfsi-V4UN6bnb2Z-0",
+        authDomain: "mori-vocab-app.firebaseapp.com",
+        projectId: "mori-vocab-app",
+        storageBucket: "mori-vocab-app.firebasestorage.app",
+        messagingSenderId: "808947287056",
+        appId: "1:808947287056:web:04ddcce5f3e0803a763695"
+    }
+};
